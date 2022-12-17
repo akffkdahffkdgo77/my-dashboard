b2d22c4c4e8b39d99266d510db64ec4b217d86cd
김가영 <namiein@naver.com>
2022-12-17 18:08:25 +0900

    mod : header에 useMatch 추가

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,18 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useMatch } from 'react-router-dom';
 
 export default function Header() {
+    const isHome = useMatch('/');
+
     return (
-        <header className="fixed top-0 left-0 p-5 right-[450px] min-h-[80px] bg-white flex items-center justify-end gap-10">
+        <header className="fixed top-0 left-0 p-5 right-[450px] min-h-[80px] font-mono bg-white flex items-center justify-end gap-10">
             <h1 className="flex-1 text-4xl font-bold font-mono">My Dashboard</h1>
             <ul className="w-full justify-end flex items-center gap-5 font-mono">
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link className={`${isHome ? 'font-bold' : ''}`} to="/">
+                        Home
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/">Settings</Link>
-                </li>
-                <li>
-                    <Link to="/">Help</Link>
+                    <Link to="/">My Settings</Link>
                 </li>
             </ul>
             <div className="">
