eab35405c83cf0de72b051ac56e9bbe5d99a9879
김가영 <namiein@naver.com>
2022-12-17 18:31:39 +0900

    feat : component import 파일 추가

---
+++ src/component/index.ts
@@ -0,0 +1,8 @@
+import DailyCommits from 'component/DailyCommits';
+import MonthlyCommits from 'component/MonthlyCommits';
+import Projects from 'component/Projects';
+import Skills from 'component/Skills';
+import WeeklyCloneCoding from 'component/WeeklyCloneCoding';
+import WeeklyCommits from 'component/WeeklyCommits';
+
+export { DailyCommits, MonthlyCommits, Projects, Skills, WeeklyCloneCoding, WeeklyCommits };
