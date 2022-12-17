e18b1d4c60ca7c359ae5fc2ddc8f47d05cffcf81
김가영 <namiein@naver.com>
2022-12-17 18:08:12 +0900

    mod : layout import 정리

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -2,6 +2,5 @@ import Aside from 'layouts/Aside';
 import BaseLayout from 'layouts/BaseLayout';
 import Footer from 'layouts/Footer';
 import Header from 'layouts/Header';
-import { Tab, TabIndicator, TabItem, TabList, TabPanel } from 'layouts/Tab';
 
-export { Aside, BaseLayout, Footer, Header, Tab, TabIndicator, TabItem, TabList, TabPanel };
+export { Aside, BaseLayout, Footer, Header };
