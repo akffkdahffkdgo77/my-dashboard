5a97a9e4c763bbc2ce521decb41a7fb5af3c0259
김가영 <namiein@naver.com>
2022-12-17 18:05:20 +0900

    mv : file moved

--- src/layouts/Tab/Tab/index.tsx
+++
@@ -1,8 +0,0 @@
-interface ITab {
-    children: React.ReactNode[];
-}
-
-// A set of tab elements and their associated tab panels.
-export default function Tab({ children }: ITab) {
-    return <div className="mt-5">{children}</div>;
-}
--- src/layouts/Tab/TabIndicator/index.tsx
+++
@@ -1,7 +0,0 @@
-interface ITabIndicator {
-    selected: boolean;
-}
-
-export default function TabIndicator({ selected }: ITabIndicator) {
-    return <div className={`${selected ? 'visible' : 'invisible'} transition-all w-3/4 h-[2px] bg-black mx-auto`} />;
-}
