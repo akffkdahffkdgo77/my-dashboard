51920af3fd4a2617e285d3ac40e890e23a38f00d
김가영 <namiein@naver.com>
2022-12-24 21:30:44 +0900

    mod : menu keyboard interaction 수정

--- src/common/Menu/context/Provider.tsx
+++ src/common/Menu/context/Provider.tsx
@@ -14,8 +14,7 @@ export const changeTabIndex = ({ linkRefs, tabIndex }: TabIndexType) => {
         rest.pop();
         rest.forEach((ref) => (ref.current!.tabIndex = -1));
     } else {
-        const rest = linkRefs.slice(0, linkRefs.length);
-        linkRefs.shift();
+        const rest = linkRefs.slice(1, linkRefs.length);
         rest.forEach((ref) => (ref.current!.tabIndex = -1));
     }
 };
@@ -29,9 +28,22 @@ export default function MenuButtonProvider({ children }: IMenuButtonProvider) {
     // Add Keyboard Event Listener
     useEffect(() => {
         const onKeyMove = (event: KeyboardEvent) => {
-            if ((event.key === 'ArrowDown' || event.key === 'ArrowUp') && !isVisible) {
+            if (event.key === 'Escape') {
+                setIsVisible(false);
+            } else if ((event.key === 'ArrowDown' || event.key === 'ArrowUp') && !isVisible) {
                 setIsVisible(true);
                 setKey(event.key);
+                document.body.style.overflow = 'hidden';
+            } else if (isVisible && (event.key === 'ArrowDown' || event.key === 'ArrowUp')) {
+                linkRefs.forEach((ref) => {
+                    if (ref.current?.tabIndex) {
+                        ref.current.focus();
+                        ref.current.tabIndex = 0;
+                    } else if (ref.current && ref.current.tabIndex === 0) {
+                        ref.current.blur();
+                        ref.current.tabIndex = -1;
+                    }
+                });
             }
         };
 
@@ -66,6 +78,7 @@ export default function MenuButtonProvider({ children }: IMenuButtonProvider) {
     const onKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
         e.preventDefault();
         if (e.key === 'Enter' || e.code === 'Space') {
+            document.body.style.overflow = 'hidden';
             setIsVisible((prev) => !prev);
             setKey(e.key === 'Enter' ? 'Enter' : 'Space');
         }
@@ -82,6 +95,12 @@ export default function MenuButtonProvider({ children }: IMenuButtonProvider) {
             changeTabIndex({ linkRefs, tabIndex: 0 });
         }
     }, [isVisible, linkRefs]);
+
+    useEffect(() => {
+        if (!isVisible) {
+            document.body.style.overflow = 'auto';
+        }
+    }, [isVisible]);
 
     const value: IMenuButtonContext = useMemo(() => ({ isVisible, setIsVisible, linkRefs, onClick, onKeyDown, onMouseEnter, onMouseLeave }), [isVisible, linkRefs, onMouseEnter, onMouseLeave]);
 
